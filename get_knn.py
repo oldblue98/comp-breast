3b513e4c6dba1bd9ca27eab9e00984179a705220
oldblue98 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-05-15 01:36:43 +0900

    add get_knn

--- get_knn.py
+++ get_knn.py
@@ -16,6 +16,22 @@ options = parser.parse_args()
 CFG = json.load(open(options.config))
 config_filename = os.path.splitext(os.path.basename(options.config))[0]
 
+# logger の設定
+from logging import getLogger, StreamHandler,FileHandler, Formatter, DEBUG, INFO
+logger = getLogger("logger")    #logger名loggerを取得
+logger.setLevel(DEBUG)  #loggerとしてはDEBUGで
+#handler1を作成
+handler_stream = StreamHandler()
+handler_stream.setLevel(DEBUG)
+handler_stream.setFormatter(Formatter("%(asctime)s: %(message)s"))
+#handler2を作成
+handler_file = FileHandler(filename=f'./logs/inference_{config_filename}_{CFG["model_arch"]}.log')
+handler_file.setLevel(DEBUG)
+handler_file.setFormatter(Formatter("%(asctime)s: %(message)s"))
+#loggerに2つのハンドラを設定
+logger.addHandler(handler_stream)
+logger.addHandler(handler_file)
+
 th = options.th
 knn = options.knn
 
@@ -38,18 +54,19 @@ def main():
     oof_df = oof_df.groupby("id").apply(get_knn)
     oof_f1score = f1_score(oof_df.label, oof_df.valid)
     oof_acc_score = accuracy_score(oof_df.label, oof_df.valid)
+    logger.debug(f'oof_f1: {oof_f1score}, oof_acc: {oof_acc_score}')
 
     test_df = pd.DataFrame()
     df1 = pd.read_csv(f'./data/output/submission_{config_filename}_{CFG["model_arch"]}.csv')
     _path = ','.join(df1.Id)
     pattern = '([0-9]*?)_x([0-9]*?)_y([0-9]*?).png'
     results = re.findall(pattern, _path, re.S)
-    df2 = pd.DataFrame(results, columns=["Id", "x", "y"])
+    df2 = pd.DataFrame(results, columns=["id", "x", "y"])
     df3 = pd.concat([df2, df1], axis=1)
     test_df = pd.concat([test_df, df3], axis=0)
     test_df.x = test_df.x.astype(int)//50
     test_df.y = test_df.y.astype(int)//50
-    test_df = test_df.groupby("Id").apply(get_knn)
+    test_df = test_df.groupby("id").apply(get_knn)
 
     sub = pd.read_csv("./data/input/submission.csv")
     sub = sub.sort_values('Id').reset_index(drop=True)
