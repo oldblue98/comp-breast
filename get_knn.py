f3fc729cc412fde4173a64fca7becadde4fa083a
oldblue98 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-05-15 18:30:21 +0900

    modefied get knn

--- get_knn.py
+++ get_knn.py
@@ -49,6 +49,8 @@ def main():
     oof_df = train_df.copy()
     oof_df["label"] = oof_csv.loc[:, ["0", "1"]].idxmax(axis=1)
     oof_df.label = oof_df.label.astype(int)
+    oof_df.x = oof_df.x.astype(float)//50
+    oof_df.y = oof_df.y.astype(float)//50
     oof_df = oof_df.groupby("id").apply(get_knn)
     oof_f1score = f1_score(oof_df.label, oof_df.valid)
     oof_acc_score = accuracy_score(oof_df.label, oof_df.valid)
